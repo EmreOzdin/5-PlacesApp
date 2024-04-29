352f682b4be27fbd6aa994b9c5866c8f4c6b17a4
Emre Özdin <mrzdnboun@gmail.com>
2024-04-29 12:13:46 +0300

    "Add getAddress function to util/location.js and use it in LocationPicker.js to get address of picked location"

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -13,7 +13,7 @@ import {
 import { Colors } from "../../constants/colors";
 import OutlinedButton from "../UI/OutlinedButton.js";
 import { useEffect, useState } from "react";
-import { getMapPreview } from "../../util/location.js";
+import { getAddress, getMapPreview } from "../../util/location.js";
 
 function LocationPicker({ onPickLocation }) {
   const [pickedLocation, setPickedLocation] = useState();
@@ -34,6 +34,19 @@ function LocationPicker({ onPickLocation }) {
       setPickedLocation(mapPickedLocation);
     }
   }, [route, isFocused]);
+
+  useEffect(() => {
+    async function handleLocation() {
+      if (pickedLocation) {
+        const address = await getAddress(
+          pickedLocation.lat,
+          pickedLocation.lng
+        );
+        onPickLocation({ ...pickedLocation, address: address });
+      }
+    }
+    handleLocation();
+  }, [pickedLocation, onPickLocation]);
 
   useEffect(() => {
     onPickLocation(pickedLocation);
