4bcf7b4ebe0f07d3c7514d3f44cc5289bd3116e1
Emre Özdin <mrzdnboun@gmail.com>
2024-05-03 14:08:53 +0300

    "Add styles for PlaceItem component"
    This commit adds styles for the PlaceItem component, including styles for the item container, pressed state, image, info section, title, and address. The styles are defined using the StyleSheet.create

--- components/Places/PlaceItem.js
+++ components/Places/PlaceItem.js
@@ -1,4 +1,5 @@
 import { View, Text, Image, Pressable, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
 
 const PlaceItem = ({ place, onSelect }) => {
   return (
@@ -14,4 +15,39 @@ const PlaceItem = ({ place, onSelect }) => {
 
 export default PlaceItem;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    borderRadius: 6,
+    backgroundColor: Colors.primary500,
+    elevation: 2,
+    elevation: 2,
+    shadowColor: "black",
+    shadowOpacity: 0.15,
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 2,
+  },
+  pressed: {
+    opacity: 0.9,
+  },
+  image: {
+    flex: 1,
+    borderBottomLeftRadius: 4,
+    borderBottomRightRadius: 4,
+    height: 100,
+  },
+  info: {
+    flex: 2,
+    padding: 12,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: Colors.gray700,
+  },
+  address: {
+    fontSize: 16,
+    color: Colors.gray700,
+  },
+});
