cc8203e8d20b5e95439830805f0a4c36bbb04174
Emre Özdin <mrzdnboun@gmail.com>
2024-05-03 14:00:42 +0300

    "Update PlaceForm to pass place data to AddPlace, adjust Place model and PlaceForm usage, handle place creation in AddPlace, and display created place in AllPlaces"

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,5 +1,16 @@
+import { useEffect, useState } from "react";
 import PlacesList from "../components/Places/PlacesList";
+import { useIsFocused } from "@react-navigation/native";
 
-export default function AllPlaces() {
-  return <PlacesList />;
+export default function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadaedPlaces] = useState();
+
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadaedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, []);
+
+  return <PlacesList places={loadedPlaces} />;
 }
