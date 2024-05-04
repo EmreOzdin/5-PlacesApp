c30b6e7c62818174c6a2be665bcb8c40fae62d82
Emre Özdin <mrzdnboun@gmail.com>
2024-05-04 23:06:30 +0300

    "Add loading state for PlaceDetails screen and fetch place details in useEffect"
    This diff includes the following changes:
    
    * In `App.js`, the title for the PlaceDetails screen is set to "Loading Place ..." when

--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -1,22 +1,42 @@
-import { Image, ScrollView, StyleSheet, Text } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import OutlinedButton from "../components/UI/OutlinedButton";
 import { Colors } from "../constants/colors";
+import { fetchPlaceDetails } from "../util/database";
+import { useState } from "react";
 
-function PlaceDetails({ route }) {
+function PlaceDetails({ route, navigation }) {
+  const [fetchedPlace, setFetchedPlace] = useState();
+
   function showOnMapHandler() {}
 
   const selectedPlaceId = route.params.placeId;
 
   useEffect(() => {
-    //use selectedPlaceId to fetch data for a single place
+    async function loadPlaceData() {
+      const place = await fetchPlaceDetails(selectedPlaceId);
+      setFetchedPlace(place);
+      navigation.setOptions({
+        title: place.title,
+      });
+    }
+
+    loadPlaceData();
   }, [selectedPlaceId]);
+
+  if (!fetchedPlace) {
+    return (
+      <View style={styles.fallback}>
+        <Text>Loading place data..</Text>
+      </View>
+    );
+  }
 
   return (
     <ScrollView>
-      <Image style={styles.image} />
+      <Image style={styles.image} source={{ uri: fetchedPlace.imageUri }} />
       <View style={styles.locationContainer}>
         <View style={styles.addressContainer}>
-          <Text style={styles.address}>ADDRESS</Text>
+          <Text style={styles.address}>{fetchedPlace.address}</Text>
         </View>
       </View>
       <OutlinedButton icon="map" onPress={showOnMapHandler}></OutlinedButton>
@@ -26,7 +46,12 @@ function PlaceDetails({ route }) {
 
 export default PlaceDetails;
 
-const styles = StyleSheet.create()({
+const styles = StyleSheet.create({
+  fallback: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   image: {
     height: "35%",
     minHeight: 300,
