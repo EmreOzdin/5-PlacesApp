119b262b05413375133a86edc8150c0e8e93e03b
Emre Özdin <mrzdnboun@gmail.com>
2024-05-04 22:32:10 +0300

    "Add place id to Place model, update AddPlace and AllPlaces screens to handle new id, and implement fetchPlaces utility function"

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 import PlacesList from "../components/Places/PlacesList";
 import { useIsFocused } from "@react-navigation/native";
+import { fetchPlaces } from "../util/database";
 
 export default function AllPlaces({ route }) {
   const [loadedPlaces, setLoadaedPlaces] = useState();
 
   const isFocused = useIsFocused();
   useEffect(() => {
-    if (isFocused && route.params) {
-      setLoadaedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    async function loadPlaces() {
+      const places = await fetchPlaces();
+      setLoadaedPlaces(places);
     }
-  }, []);
+
+    if (isFocused) {
+      loadPlaces();
+      //setLoadaedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, [isFocused]);
 
   return <PlacesList places={loadedPlaces} />;
 }
